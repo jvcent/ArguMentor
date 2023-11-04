0b3f2b7b4c8ff877f8d987c223a0d823de1ff8bc
Yenah Lee <yenah.lee27@gmail.com>
2023-11-04 13:18:34 -0400

    Minor modifications to App.js & Landing.jsx

--- client/src/pages/Input.jsx
+++ client/src/pages/Input.jsx
@@ -31,4 +31,5 @@ export const Input = () => {
             </div>
         </>
     )
+    
 }
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -4,11 +4,21 @@ export const Landing = () => {
     const navigate = useNavigate();
 
     const handleButtonClick = () => {
-        navigate("/input-page")
+        navigate("/input")
     }
     return (
-        <div>
-            <button onClick={handleButtonClick}>Get Started</button>
+        <>
+        <div class="flex justify-center items-center h-screen bg-blue-800">
+            <button class="cursor-pointer transition-all 
+        bg-white text-indigo-900 px-10 py-4 rounded-lg
+        border-purple-400
+        border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
+        active:border-b-[2px] active:brightness-90 active:translate-y-[2px] hover:shadow-xl hover:shadow-purple-500 shadow-purple-500 active:shadow-none"onClick={handleButtonClick}>
+            Get Started
+        </button>
         </div>
+        </>
+    
     )
 }
+
