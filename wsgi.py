2a01390c9bbd6072fda72da8bbf6ee33a1f7b5ea
michaelZhaoCode <82855036+michaelZhaoCode@users.noreply.github.com>
2023-11-04 18:16:01 -0400

    trying ssl

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from app.main import app
 
 if __name__ == "__main__":
-  app.run(debug = True, ssl_context=('app/cert.pem', 'app/key.pem'))
+  # app.run(debug = True, ssl_context=('app/cert.pem', 'app/key.pem'))
+  app.run(debug=True)
