1a226b60c2a2757659c8761ca2534fa926dcb525
Jon Vincentius <jon.vincentius@mail.utoronto.ca>
2023-11-05 05:35:11 -0500

    Input component UI/UX

--- client/src/pages/Input.jsx
+++ client/src/pages/Input.jsx
@@ -100,17 +100,22 @@ export const Input = () => {
     return (
         <>
             <div className="main-font bg-blue-800 flex flex-col items-center justify-center h-screen">
-                <p className="text-white text-lg mb-8">Instructions: Upload a PDF of your course notes/lecture notes or type in a specific topic that you want to practice studying.</p>
-                {pdfUploaded ? ( // Render message when PDF is uploaded
-                    <p className="text-white text-lg mb-8">PDF Successfully Uploaded!</p>
-                    ) : (
-                    <div {...getRootProps()}>
-                        <input {...getInputProps()} />
-                        <button className="bg-white mb-8 mt-2 text-blue-800 px-6 py-2 rounded-full transition duration-200 ease-in-out hover:bg-blue-100 active:bg-blue-100 focus:outline-none">Click to select a file</button>
-                    </div>
-                    )}
-                <input type="text" onChange={handleUserMessage} className="bg-white mb-8 mt-2 text-blue-800 px-7 py-2 rounded-full transition duration-200 ease-in-out hover:bg-blue-100 active:bg-blue-100 focus:outline-none" placeholder="Type in a specific topic"></input>
-                <button onClick={handleButtonClick} className="mt-5 relative py-2 px-8 text-black text-base font-bold uppercase rounded-[50px] overflow-hidden bg-white transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-violet-400 before:to-blue-500 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-[50px] hover:before:left-0">Start Practicing</button>
+                <p className="text-white text-xl font-medium mb-6 ">Upload a PDF of your course notes or type in a specific topic that you want to revise</p>
+                <div className="w-full flex flex-row justify-center space-x-4">
+                <input type="text" onChange={handleUserMessage} className="bg-white mb-8 mt-2 w-1/3 text-lg drop-shadow-xl font-semibold text-blue-800 px-7 py-2 rounded-xl transition duration-200 ease-in-out focus:outline-none" placeholder="Enter a specific topic"></input>
+                  <div>
+                    {pdfUploaded ? ( // Render message when PDF is uploaded
+                      <p className="text-white text-lg mb-8">PDF Successfully Uploaded!</p>
+                      ) : (
+                      <div {...getRootProps()}>
+                          <input {...getInputProps()} />
+                          <button className="bg-white mb-8 mt-2 text-blue-800 px-6 py-4 rounded-full transition duration-200 ease-in-out hover:bg-blue-100 active:bg-blue-100 focus:outline-none">Upload file</button>
+                      </div>
+                      )}
+                  </div>
+                </div>
+                
+                <button onClick={handleButtonClick} className="mt-5 relative py-6 px-10 text-black text-base font-bold uppercase rounded-[50px] overflow-hidden bg-white transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-violet-400 before:to-blue-500 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-[50px] hover:before:left-0">Start Revising</button>
             </div>
         </>
     )
